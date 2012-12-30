f7406c8d1906ffcce1ff6e2313d37b5a7f6bf1a3
Federico Jaramillo <jm.federico@yahoo.com>
2012-12-30 18:12:01 -0500

    MAke it generic for any form
    Change the module to:
     - Use PHP a generated seed
     - Be usable with any form
     - Give more details about why validation failed
     - Add extra validation (non empty field)

--- js/badbot.js
+++ js/badbot.js
@@ -5,31 +5,19 @@ Drupal.behaviors.badbot = {
     if (Drupal.settings.badbot.forms.length) {
       $(Drupal.settings.badbot.forms).each(function(i, form_data) {
 
-        var form = $('#' + form_data.form_id);
-        if (form.length) {
-          var generated = false;
-          
-          form.submit(function(e) {
-            if (!generated) {
-              e.preventDefault();
-              
-              generated = true;
-              
-              var field_data = $('#edit-' + form_data.field, form).attr('value');
-            
-              if(field_data && $('#edit-' + form_data.validation_field, form)) {
-                $.get(Drupal.settings.badbot.base_path + '/badbot/token/' + field_data, function(return_data){
-                  $('#edit-' + form_data.validation_field, form).attr('value', return_data);
-                  
-                  form.submit();
-                });
-              }
-            }
-          });
-        }
+        $('#' + form_data.form_id).once('badbot', function() {
+          $form = $(this);
+          var field_data = $('[name=' + form_data.field + ']', $form).attr('value');
+
+          if (field_data && $('[name=' + form_data.validation_field + ']', $form)) {
+            $.get(Drupal.settings.badbot.base_path + '/badbot/token/' + field_data, function(return_data){
+              $('[name=' + form_data.validation_field + ']', $form).attr('value', return_data);
+            });
+          }
+        });
       });
     }
-  }  
+  }
 };
 
 })(jQuery);
